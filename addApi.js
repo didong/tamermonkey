b07f130ea4a3c6da938339b008ed48787551abf0
didong <670080087@qq,com>
2022-11-06 21:00:33 +0800

    第一个测试脚本

---
+++ addApi.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         融合网关接口新增
+// @namespace    http://didong.space
+// @version      0.1
+// @description  专有云融合网关接口新增
+// @author       didong
+// @match        <$URL$>
+// @icon         <$ICON$>
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
